84e6ecb9b1eab0f7bcc4c49e4977a4ce51b5abfe
Ubinquitous <ubinquitous1@gmail.com>
2023-02-03 15:03:01 +0900

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by 박우빈 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
